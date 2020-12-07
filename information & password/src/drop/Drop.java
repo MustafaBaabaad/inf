89944511e85efedbef292e5a20e7347fc9331e46
mustafa <st.e734267636@gmail.com>
2020-12-07 21:22:27 +0300

    first commit

---
+++ information & password/src/drop/Drop.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drop;
+
+/**
+ *
+ * @author umb
+ */
+public class Drop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame1 f =new Frame1();
+        f.setVisible(true);
+        frame2 f2;
+    }
+    
+}
